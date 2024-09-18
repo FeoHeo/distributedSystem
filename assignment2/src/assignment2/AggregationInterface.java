0bbcfff6aa27cb3c04b48f92c374d14ced3c0042
FeoHeo <nguyenduong8bc1@gmail.com>
2024-09-18 16:35:48 +0930

    Updated todo

--- assignment2/src/assignment2/AggregationInterface.java
+++
@@ -1,8 +0,0 @@
-package assignment2;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface AggregationInterface extends Remote {
-	String helloTest() throws RemoteException;
-}
